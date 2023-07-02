1930c634dd93221801c38f2e6e4764a3fcbe42bb
Anjesh-bit <75300382+Anjesh-bit@users.noreply.github.com>
2023-07-02 09:03:27 +0545

    Update DrawerComponentActions.jsx
    Refractor

--- src/actions/DrawerComponentActions.jsx
+++ src/actions/DrawerComponentActions.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { ON_DRAWER_OPEN, ON_DRAWER_FAIL } from "../constants/DrawerComponentConstants";
 
-const DrawerComponentActions = (anchorDrawer, open) => (dispatch) => {
+const drawerComponentActions = (anchorDrawer, open) => (dispatch) => {
     try {
         dispatch({ type: ON_DRAWER_OPEN, [anchorDrawer]: open });
     }
@@ -14,6 +14,4 @@ const DrawerComponentActions = (anchorDrawer, open) => (dispatch) => {
         })
     }
 }
-
-
-export { DrawerComponentActions }
+export { drawerComponentActions }
