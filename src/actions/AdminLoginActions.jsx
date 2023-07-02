2d606b9f571f59e186c48cf43bdcb7dc0e6c89b9
Anjesh-bit <75300382+Anjesh-bit@users.noreply.github.com>
2023-07-02 08:09:32 +0545

    Update AdminLoginActions.jsx
    Refractor

--- src/actions/AdminLoginActions.jsx
+++ src/actions/AdminLoginActions.jsx
@@ -3,43 +3,45 @@ import axios from "axios";
 import { deleteCookies } from "../utils/Cookie";
 import { deleteLocalStorage } from "../utils/Localstorage";
 import { auth } from "../utils/Auth";
-import { ADMIN_LOGIN_START, ADMIN_LOGIN_FAIL, ADMIN_LOGIN_SUCCESS, ADMIN_LOGOUT } from "../constants/AdminLoginConstants";
+import {
+  ADMIN_LOGIN_START,
+  ADMIN_LOGIN_FAIL,
+  ADMIN_LOGIN_SUCCESS,
+  ADMIN_LOGOUT,
+} from "../constants/AdminLoginConstants";
 
 const Login = axios.create({
-    baseURL: "http://localhost:5000/"
-})
+  baseURL: "http://localhost:5000/",
+});
 
-const adminLoginActions = (email, password) => async (dispatch) => {
-    try {
-        dispatch({ type: ADMIN_LOGIN_START })
-        const config = {
-            headers: {
-                "Content-Type": "application/json",
-            }
-        }
-        const { data } = await Login.post("greenApi/admin/login", { email, password }, config);
-        dispatch({ type: ADMIN_LOGIN_SUCCESS, payload: data });
-        
-        const Logintoken = data.token;
-        auth(Logintoken, data);       
-    }
-    catch (error) {
-        dispatch(
-            {
-                type: ADMIN_LOGIN_FAIL,
-                payload: error.response.data.message
-                    ? error.response.data.message
-                    : error.message,
-            })
-    }
-}
+const adminLoginActions = (adminEmail, adminPassword) => async (dispatch) => {
+  try {
+    dispatch({ type: ADMIN_LOGIN_START });
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const { data } = await Login.post("/greenApi/admin/login", { adminEmail, adminPassword }, config);
+    dispatch({ type: ADMIN_LOGIN_SUCCESS, payload: data });
 
+    const loginToken = data.token;
+    auth(loginToken, data);
+  } catch (error) {
+    dispatch({
+      type: ADMIN_LOGIN_FAIL,
+      payload: error.response.data.message
+        ? error.response.data.message
+        : error.message,
+    });
+  }
+};
 
 const adminLogoutActions = () => (dispatch) => {
-    deleteCookies("token");
-    deleteLocalStorage("loginData");
-    deleteLocalStorage("userInfo");
-    dispatch({ type: ADMIN_LOGOUT })
-}
+  deleteCookies("token");
+  deleteLocalStorage("loginData");
+  deleteLocalStorage("userInfo");
+  dispatch({ type: ADMIN_LOGOUT });
+};
 
 export { adminLoginActions, adminLogoutActions };
